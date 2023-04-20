76f327ba6113f76ecb167be80f974fd5ea179cf8
shubham <pghotkar931@gmail.com>
2023-04-20 17:14:45 +0530

    feature ;on click button

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
+//
+const button = document.querySelector(".btn");
+const output = document.querySelector(".show--output");
+button.addEventListener("click", addClickEvent);
 
+let arr = [
+  {
+    message: "Message Lorem ipsum dolor sit amet.",
+    color: "red",
+  },
+  {
+    message: "Message Lorem ipsum dolor.",
+    color: "green",
+  },
+  {
+    message: "Message  ipsum dolor sit amet.",
+    color: "purple",
+  },
+  {
+    message: "Message Lorem ipsum dolor sit amet amet amet.",
+    color: "orange",
+  },
+];
 
+function addClickEvent() {
+  //to show output from array
+  let index = Math.floor(Math.random() * arr.length);
+  const { message, color } = arr[index];
 
-const button = document.querySelector(".btn")
+  //create Element
+  let paraElement = document.createElement("p");
+
+  // assign value to new Element
+  paraElement.textContent = message;
+  paraElement.style.color = color;
+  output.appendChild(paraElement);
+
+  //for remove element
+  setTimeout(() => {
+    paraElement.remove();
+  }, 3000);
+}
